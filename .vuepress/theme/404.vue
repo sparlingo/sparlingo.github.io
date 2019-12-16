26effc94a473acf161af43d4eedac44788474528
Kevin Sparling <kevinsparling@gmail.com>
2019-12-16 19:19:37 +0800

    added 404 page

---
+++ .vuepress/theme/404.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="theme-container">
+    <Navbar />
+    <section class="notFound">
+        <div class="container">
+            <h1 class="title is-1">
+                Sorry, that isn't available
+            </h1>
+        </div>
+    </section>
+    <Footer/>
+  </div>
+</template>
